021341de83bc9693ae64cc76306d129b571d1660
Nikita Baranovskiy <nikita.baranovskiy.03@gmail.com>
2025-06-06 21:35:09 +0300

    mongo New 3

--- blogers_platform/src/core/settings/settings.ts
+++ blogers_platform/src/core/settings/settings.ts
@@ -1,5 +1,5 @@
 export const SETTINGS = {
     DB_NAME: "Blogger_platform",
-    MONGO_URL:"mongodb://localhost:27017/TEST",
+    MONGO_URL:"mongodb://admin:admin@ac-dzt7wug-shard-00-00.kj4rash.mongodb.net:27017,ac-dzt7wug-shard-00-01.kj4rash.mongodb.net:27017,ac-dzt7wug-shard-00-02.kj4rash.mongodb.net:27017/?ssl=true&replicaSet=atlas-9b68xv-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster",
     PORT:27017
 }
